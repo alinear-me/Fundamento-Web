562ea563fa59cf181de43370237ba25311a3e62a
alinear-me <aline.monteiro100@gmail.com>
2021-08-18 19:28:32 -0300

    Primeiro código em JS

---
+++ asserts/js/script.js
@@ -0,0 +1,45 @@
+/*
+case sensitive: reconhece letras masiúsculas e minusculas
+por tag: getElementByName()
+por id: getElementById()
+por nome: getElementsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector()
+
+*/
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome inválido'
+        txtNome.style.color = 'red'
+         
+    }
+
+    else { 
+        txtNome.innerHTML = 'Nome válido'
+        txtNome.style.color = 'green'
+         
+    }
+}
+
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    }
+    else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green'
+    }
+    
+}
